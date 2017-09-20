42c3042b9af3d871f86ba97de6adbc367f42ddd7
Tuomas-Matti Soikkeli <tuomas-matti.soikkeli@sc5.io>
2017-09-20 17:24:40 +0300

    Add first stuffs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Header from './components/header';
+
+const App = function() {
+  return (
+    <div>
+      <Header/>
+      SC5 IoT Hub Test application
+    </div>
+  );
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
